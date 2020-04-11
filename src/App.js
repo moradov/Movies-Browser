3ca59d138bd6a04acd134b8e88ebfe2969c94f1c
Morad <moradazyzy@gmail.com>
2020-04-11 16:15:45 +0100

    no msg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Fragment, useContext } from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
 import globalContext from "./context/global/globalContext";
-import { Provider } from "react-redux";
 import Navbar from "./component/UI/Navbar/Navbar";
 import Home from "./component/Home/Home";
 import Search from "./pages/Search/Search";
 import Model from "./component/UI/Model/Model";
-import store from "./store";
-
 const App = () => {
   const { model } = useContext(globalContext);
   return (
     <Fragment>
-      <Provider store={store}>
-        <Navbar />
-        {model.display ? <Model msg={model.msg} /> : null}
-        <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/search' exact component={Search} />
-        </Switch>
-      </Provider>
+      <Navbar />
+      {model.display ? <Model msg={model.msg} /> : null}
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/search' exact component={Search} />
+      </Switch>
     </Fragment>
   );
 };
--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -7,17 +7,11 @@ import DefaultJobsSection from "../JobsSecton/JobsSection";
 import MembersCard from "../../component/MembersCard/MembersCard";
 import Contact from "../../component/Contact/Contact";
 import Footer from "../../component/Footer/Footer";
-import { connect } from "react-redux";
-import { changeTestOne } from "../../actions/action1";
 const Home = props => {
   const { getDefaultJobs } = useContext(GlobalContext);
-  // getDefaultJobs();
-  const btnClicked = () => {
-    props.changeTestOne;
-  };
+  useEffect(() => getDefaultJobs(), []);
   return (
     <Fragment>
-      <button onClick={btnClicked}>cickkkkkkkkkkkkkkkk </button>
       <Banner />
       <Features />
       <Stat />
@@ -28,12 +22,5 @@ const Home = props => {
     </Fragment>
   );
 };
-const mapStateToProps = state => {
-  return {
-    call: state.state1
-  };
-};
-// const actions = (dispatch)=>{
-//   ()
-// }
-export default connect(mapStateToProps, { changeTestOne })(Home);
+
+export default Home;
